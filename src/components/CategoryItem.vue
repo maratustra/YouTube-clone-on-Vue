48de2e0d435c81b8a8855dccdbb6b0c1b0aea61f
Alena Maramygina <maratustraa@gmail.com>
2022-05-13 23:23:46 +0400

    added category item component with interactive styles

---
+++ src/components/CategoryItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <a href="#" :class="classes">
+    {{ category }}
+  </a>
+</template>
+
+<script>
+export default {
+  name: "CategoryItem",
+  props: ['category', 'isActive'],
+  computed: {
+    classes() {
+      // классы, которые применяются ко всем категориям
+      const classes = ['px-3', 'py-1', 'transition', 'border', 'rounded-full']
+      // значение isActive зависит от переданного значения isActive извне
+      return this.isActive ? [
+          ...classes, // добавляем классы для активной категории
+        'bg-gray-600',
+        'border-gray-700',
+        'hover:border-gray-200',
+        'text-white'
+      ] : [
+          ...classes, // либо для неактивной
+        'bg-gray-100',
+        'border-gray-300',
+        'hover:bg-gray-200',
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/TheCategories.vue
+++ src/components/TheCategories.vue
@@ -2,17 +2,23 @@
   <section class="fixed w-full xl:pl-64 md:pl-24 pt-14 bg-white bg-opacity-95 z-10">
     <div class="max-w-screen-2xl m-auto px-4 border-t border-b">
       <div class="flex py-3 space-x-3 overflow-auto text-sm whitespace-nowrap">
-        <a v-for="category in categories" :key="category" href="#" class="px-3 py-1 transition bg-gray-600 border border-gray-700 rounded-full hover:border-gray-200 text-white">
-          {{ category }}
-        </a>
+        <category-item
+            v-for="category in categories"
+            :key="category"
+            :category="category"
+            :is-active="category === 'All'"
+        />
       </div>
     </div>
   </section>
 </template>
 
 <script>
+import CategoryItem from "./CategoryItem.vue";
+
 export default {
   name: "TheCategories",
+  components: {CategoryItem},
   data() {
     return {
       categories: [
