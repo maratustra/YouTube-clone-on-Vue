b6f2236848822282a9fcc5ebfbdb878650538557
Alena Maramygina <maratustraa@gmail.com>
2022-05-22 01:17:00 +0400

    adding components for sidebar in process

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -2,40 +2,15 @@
   <aside class="hidden xl:block w-64 max-h-screen overflow-auto fixed top-12 bg-white z-20">
     <section class="py-2 border-b">
       <ul>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm font-medium text-gray-800 bg-gray-200 hover:bg-gray-300">
-            <svg class="w-6 h-6 mr-6 text-red-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
-            <span>Home</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm hover:bg-gray-100">
-            <svg class="w-6 h-6 mr-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z"></path></svg>
-            <span>Trending</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm hover:bg-gray-100">
-            <svg class="w-6 h-6 mr-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"></path></svg>
-            <span>Subscriptions</span>
-          </a>
-        </li>
+        <sidebar-nav-item label="Home" icon="home" is-active />
+        <sidebar-nav-item label="Trending" icon="trending" />
+        <sidebar-nav-item label="Subscriptions" icon="subscriptions" />
       </ul>
     </section>
     <section class="py-2 border-b">
       <ul>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm hover:bg-gray-100">
-            <svg class="w-6 h-6 mr-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path></svg>
-            <span>Library</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm hover:bg-gray-100">
-            <svg class="w-6 h-6 mr-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-            <span>History</span>
-          </a>
-        </li>
+        <sidebar-nav-item label="Library" icon="library" />
+        <sidebar-nav-item label="History" icon="history" />
       </ul>
     </section>
     <section class="px-8 py-4 border-b font-medium leading-5">
@@ -192,8 +167,11 @@
 </template>
 
 <script>
+import SidebarNavItem from "@/components/SidebarNavItem.vue";
+
 export default {
-  name: "TheSidebar"
+  name: "TheSidebar",
+  components: {SidebarNavItem}
 }
 </script>
 
--- src/icons.js
+++ src/icons.js
@@ -19,4 +19,9 @@ export default {
     clock: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>',
     menuAlt3: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>',
     checkCircle: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>',
+    home: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>',
+    trending: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z"></path>',
+    subscriptions: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"></path>',
+    library: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path>',
+    history: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>',
 }
