0c7469da3e142b2d79625c4a647e03e773577f7e
Alena Maramygina <maratustraa@gmail.com>
2022-06-13 23:04:50 +0400

    added component for mobile sidebar

---
+++ src/components/TheSidebarMobileOverlay.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="fixed inset-0 bg-black bg-opacity-50 z-30">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheSidebarMobileOverlay",
+}
+</script>
+
+<style scoped>
+
+</style>
