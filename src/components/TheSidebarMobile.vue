32849adf1fe304fef95951d6aee3cc21c0f4ea45
Alena Maramygina <maratustraa@gmail.com>
2022-06-14 23:18:24 +0400

    added animation to close sidebar slightly

--- src/components/TheSidebarMobile.vue
+++ src/components/TheSidebarMobile.vue
@@ -1,15 +1,33 @@
 <template>
-  <the-sidebar-mobile-overlay>
-    <aside class="w-64 max-h-screen overflow-auto bg-white">
+  <transition
+      enter-from-class="opacity-0"
+      enter-active-class="transition-opacity ease-linear duration-200"
+      enter-to-class="opacity-100"
+      leave-from-class="opacity-100"
+      leave-active-class="transition-opacity ease-linear duration-200"
+      leave-to-class="opacity-0"
+  >
+    <the-sidebar-mobile-overlay @click="isOpen = false" v-show="isOpen" />
+  </transition>
+
+  <transition
+      enter-from-class="-translate-x-full"
+      enter-active-class="transition ease-in-out duration-200 transform"
+      enter-to-class="translate-x-0"
+      leave-from-class="translate-x-0"
+      leave-active-class="transition ease-in-out duration-200 transform"
+      leave-to-class="-translate-x-full"
+  >
+    <aside v-show="isOpen" class="w-64 max-h-screen overflow-auto bg-white fixed z-40">
       <section class="sticky top-0 flex items-center p-4 border-b bg-white">
-        <button class="ml-2 mr-6">
+        <button @click="isOpen = false" class="ml-2 mr-6">
           <base-icon name="menu" />
         </button>
         <logo-main />
       </section>
       <sidebar-content />
     </aside>
-  </the-sidebar-mobile-overlay>
+  </transition>
 </template>
 
 <script>
@@ -25,6 +43,11 @@ export default {
     BaseIcon,
     LogoMain,
     SidebarContent
+  },
+  data() {
+    return {
+      isOpen: true,
+    }
   }
 }
 </script>
--- src/components/TheSidebarMobileOverlay.vue
+++ src/components/TheSidebarMobileOverlay.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="fixed inset-0 bg-black bg-opacity-50 z-30">
-    <slot />
-  </div>
+  <div class="fixed inset-0 bg-black bg-opacity-50 z-30"></div>
 </template>
 
 <script>
