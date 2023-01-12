42710eeafd4236875b2a811866190599a64b6055
Alena Maramygina <maratustraa@gmail.com>
2023-01-12 23:03:43 +0700

    анимация кнопок в блоках для видео

--- src/components/VideoItemThumbnail.vue
+++ src/components/VideoItemThumbnail.vue
@@ -1,12 +1,8 @@
 <template>
   <div class="relative">
     <img :src="`https://picsum.photos/seed/image${index}/720/404`" alt="">
-    <span class="opacity-0 group-hover:opacity-100 bg-opacity-60 absolute top-0 right-0 m-1 p-1 bg-black text-white rounded-sm">
-      <base-icon class="w-5 h-5" name="clock" />
-    </span>
-    <span class="opacity-0 group-hover:opacity-100 bg-opacity-60 absolute top-8 right-0 m-1 p-1 bg-black text-white rounded-sm">
-      <base-icon class="w-5 h-5" name="menuAlt3" />
-    </span>
+    <video-item-thumbnail-button icon="clock" label="Watch later" class="top-0" />
+    <video-item-thumbnail-button icon="menuAlt3" label="Add to queue" class="top-8" />
     <span class="opacity-100 group-hover:opacity-0 duration-500 absolute bottom-0 right-0 m-1 p-1 bg-black text-white rounded-sm text-xs font-semibold">
       6:35
     </span>
@@ -15,10 +11,11 @@
 
 <script>
 import BaseIcon from "@/components/BaseIcon.vue";
+import VideoItemThumbnailButton from "@/components/VideoItemThumbnailButton.vue";
 
 export default {
   name: "VideoItemThumbnail",
-  components: {BaseIcon},
+  components: {BaseIcon, VideoItemThumbnailButton},
   props: ['index'],
 }
 </script>
